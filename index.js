f29d123d6f876fca9baf26a1dcf68114cfd2aff8
Duong1710 <tuthu999777@gmail.com>
2024-09-18 08:56:52 +0700

    Tạo folder route cơ bản part 1

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 4000 
-
+ const routeClient = require("./routes/client/index.route");
 
 app.set('views', './views') // tìm tđến thư mục tên là views
-app.set('view engine', 'pug') // temlate engine sử dụng: Pug
+app.set('view engine', 'pug') // temlate engine sử dụng: Pugn
 
-app.get('/', (req, res) => {
-  res.render("client/pages/home/index.pug");
-});
-
-
-app.get('/product', (req, res) => {
-  res.render("client/pages/products/index")
-});
-
+routeClient.index(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
