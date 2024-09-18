13b003ecc46b39e32460168041e16f219715011f
Duong1710 <tuthu999777@gmail.com>
2024-09-18 09:48:46 +0700

    Folder route part 2

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,10 @@
-module.exports.index = (app) => {
-    app.get('/', (req, res) => {
-        res.render("client/pages/home/index.pug");
-    });
 
-    app.get('/product', (req, res) => {
-        res.render("client/pages/products/index")
-    });
+const productRoute = require("./product.route");
+const homeRoute = require("./home.route");
+
+module.exports.index = (app) => {
+    app.use('/', homeRoute);
+
+    app.use('/product', productRoute);
+       
 }
---
+++ routes/client/product.route.js
@@ -0,0 +1,24 @@
+
+const express = require("express");
+const router = express.Router();
+
+
+
+
+router.get('/', (req, res) => {
+    res.render("client/pages/products/index")
+});
+
+router.get('/create', (req, res) => {
+    res.render("client/pages/products/index")
+});
+
+router.get('/edit', (req, res) => {
+    res.render("client/pages/products/index")
+});
+
+router.get('/delete', (req, res) => {
+    res.render("client/pages/products/index")
+});
+
+module.exports = router;
