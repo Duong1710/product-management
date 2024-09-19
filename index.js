bd40ba46121d39b503dc557a3044b0fe18112e1c
Duong1710 <tuthu999777@gmail.com>
2024-09-19 10:55:50 +0700

    Sửa layout

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const app = express(); 
 const port = process.env.PORT;
  const routeClient = require("./routes/client/index.route");
+ app.use(express.static('public')); // Thiết lập thư mục chứa file tĩnh
 
 app.set('views', './views') // tìm tđến thư mục tên là views
 app.set('view engine', 'pug') // temlate engine sử dụng: Pugn
