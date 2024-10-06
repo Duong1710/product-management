cde60694464c7d099439e52c780aeb929f987c98
Duong1710 <tuthu999777@gmail.com>
2024-10-06 15:47:43 +0700

    Chức năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -8,7 +8,19 @@ module.exports.index = async (req, res) => {
   if(req.query.status) {
     find.status = req.query.status;
   }
-  // Hết Lọc theo trạng thái
+   // Hết Lọc theo trạng thái
+
+   // Lọc theo title
+   if(req.query.keyword) {  
+    const regex = new RegExp(req.query.keyword,"i"); 
+    // tìm kiếm tương đối (có 1 số biểu hiện là tìm được rồi) và không phân biệt chữ hoa, chữ thường
+    find.title = regex;
+  }
+  // Hết Lọc theo title  
+  // Giải thích mấy cái lọc
+  // nếu mà trên cái dòng tìm localhost ấy sau
+  //   dấu ? có cái giá trị của keyword thì biến const find bên trên chỗ index
+  //   kia kìa sẽ có thêm cái giá trị title và người ta sẽ lọc theo cái title đó
   const products = await Product.find(find);
 
   res.render("admin/pages/products/index", {
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,28 +1,5 @@
+
 // Bộ lọc
-// const boxFilter = document.querySelector("[box-filter]");
-// if(boxFilter) {
-//   let url = new URL(location.href); // Nhân bản url
-//   // Bắt sự kiện onChange
-//   boxFilter.addEventListener("change", () => {
-//     const value = boxFilter.value;
-    
-//     if(value) {
-//       url.searchParams.set("status", value);
-//     } else {
-//       url.searchParams.delete("status");
-//     }
-//     location.href = url.href;
-//   })
-//   // Hiển thị lựa chọn mặc định
-//   const statusCurrent = url.searchParams.get("status");
-//   if(statusCurrent) {
-//     boxFilter.value = statusCurrent;
-//   }
-// }
-// Hết Bộ lọc
-
-
-
 const boxFilter = document.querySelector("[box-filter]");
 if(boxFilter){
     let url = new URL(location.href); // Nhân bản url
@@ -37,8 +14,33 @@ if(boxFilter){
         }
         location.href = url.href;
     })
+    // Hiển thị lựa chọn mặc định
     const statusCurrent = url.searchParams.get("status");
     if(statusCurrent) {
         boxFilter.value = statusCurrent;
+    }
+}
+// Hết Bộ lọc
+
+//Tìm kiếm
+const formSearch = document.querySelector("[form-search]");
+if(formSearch){
+    let url = new URL(location.href); // Nhân bản url
+
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault(); // ngăn lại việc submit form luôn
+        const value = formSearch.keyword.value;
+        if(value){
+            url.searchParams.set("keyword",value);
+        }
+        else{
+            url.searchParams.delete("keyword");
+        }
+        location.href = url.href;
+    })
+    // Hiển thị từ khóa mặc định trên màn hình
+    const valueCurrent = url.searchParams.get("keyword");
+    if(valueCurrent) {
+        formSearch.keyword.value = valueCurrent;
     }
 }
